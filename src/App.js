d45702eed754c145e4aad8296002a6d0b5fc32e2
Isak Pettersson <isak.pettersson@apoteket.se>
2024-10-18 10:47:13 +0200

    test again

--- src/App.js
+++ src/App.js
@@ -2,44 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let UPC = '';
-    document.addEventListener("keydown", function(e) {
-        const textInput = e.key || String.fromCharCode(e.key);
-        const targetName = e.target.localName;
-        let newUPC = '';
-        if (textInput && targetName !== 'input'){
-            newUPC = UPC+textInput;
-
-          if (newUPC.length >= 6) {
-            console.log('barcode scanned:  ', newUPC);
-          } 
-       }
-    });
-
-let code = "";
-let reading = false;
-
-document.addEventListener('keypress', e => {
-  //usually scanners throw an 'Enter' key at the end of read
-   if (e.key === 13) {
-          if(code.length > 5) {
-            console.log(code);
-            /// code ready to use                
-            code = "";
-         }
-    } else {
-        code += e.key; //while this is not an 'enter' it stores the every key            
-    }
-
-    //run a timeout of 200ms at the first read and clear everything
-    if(!reading) {
-        reading = true;
-        setTimeout(() => {
-            code = "";
-            reading = false;
-        }, 200);  //200 works fine for me but you can adjust it
-    }
-});
+  on_scanner() // init function
+let testValue = ''
+let testValue1= ''
+  function on_scanner() {
+      let is_event = false; // for check just one event declaration
+      let input = document.getElementById("scanner");
+      input?.addEventListener("focus", function () {
+          if (!is_event) {
+              is_event = true;
+              input.addEventListener("keypress", function (e) {
+                  setTimeout(function () {
+                      if (e.key === 13) {
+                          testValue= input.value
+                          scanner(input.value); // use value as you need
+                          input.select();
+                      }
+                  }, 500)
+              })
+          }
+      });
+      document.addEventListener("keypress", function (e) {
+          if (e.target.tagName !== "INPUT") {
+              input.focus();
+          }
+      });
+  }
+  
+  function scanner(value) {
+      if (value === '') return;
+      console.log(value)
+      testValue1 = value
+  }
 
   return (
     <div className="App">
@@ -49,14 +43,17 @@ document.addEventListener('keypress', e => {
           Edit <code>src/App.js</code> and save to reload.
           </p>
           <br/>
-          <h2>UPC is: </h2>
+          {/* <h2>UPC is: </h2>
           <input defaultValue={UPC}/>
           <p>upc: {UPC}</p>
 
-          <h2>code: </h2>>
-          <p>code: {code}</p>
-          <input defaultValue={code}/>
-        
+          <h2>code: </h2>
+          <input defaultValue={code}/> */}
+        <input type="text" id="scanner" placeholder="scanner"></input>
+        <p>code: {testValue}</p>
+        <p>code 1 {testValue1}</p>
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
