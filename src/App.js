143873af5ccfc7b5e8a89be9e14a565c9d9724ef
Isak Pettersson <isak.pettersson@apoteket.se>
2024-10-18 14:08:32 +0200

    remove

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   document.addEventListener('textInput', function (e){
@@ -18,43 +18,30 @@ document.addEventListener('keypress', e => {
   //usually scanners throw an 'Enter' key at the end of read
    if (e.key === "ENTER") {
     document.getElementById("third").innerHTML = code
-        //   if(code.length > 25) {
-        //     console.log(code);
-        //     /// code ready to use          
-        //     document.getElementById("second").innerHTML = code
-      
-        //     code = "";
-        //  }
+   
     } else {
         code += e.key; //while this is not an 'enter' it stores the every key            
         document.getElementById("first").innerHTML = code
     }
 
-    //run a timeout of 200ms at the first read and clear everything
-    // if(!reading) {
-    //     reading = true;
-    //     setTimeout(() => {
-    //         code = "";
-    //         reading = false;
-    //     }, 200);  //200 works fine for me but you can adjust it
-    // }
+    if(!reading) {
+        reading = true;
+        setTimeout(() => {
+            code = "";
+            reading = false;
+        }, 200);  //200 works fine for me but you can adjust it
+    }
 });
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <br/>
-         
-        <input type="text" id="scanner" placeholder="scanner"></input>
-        <div id="first"></div>
-        <div id="second"></div>
-        <div id="third"></div>
-        <div>{code}</div>
+       
+      <img src={logo} className="App-logo" alt="logo" />
+        <div id="first" title='first'></div>
+        <div id="second" title='second'></div>
+        <div id="third" title='third'></div>
 
         <a
           className="App-link"
