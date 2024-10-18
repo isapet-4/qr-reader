420c70587e5418f4ef0f4605c689ab5bea580a95
Isak Pettersson <isak.pettersson@apoteket.se>
2024-10-18 10:40:56 +0200

    new

--- src/App.js
+++ src/App.js
@@ -16,14 +16,47 @@ function App() {
        }
     });
 
+let code = "";
+let reading = false;
+
+document.addEventListener('keypress', e => {
+  //usually scanners throw an 'Enter' key at the end of read
+   if (e.key === 13) {
+          if(code.length > 5) {
+            console.log(code);
+            /// code ready to use                
+            code = "";
+         }
+    } else {
+        code += e.key; //while this is not an 'enter' it stores the every key            
+    }
+
+    //run a timeout of 200ms at the first read and clear everything
+    if(!reading) {
+        reading = true;
+        setTimeout(() => {
+            code = "";
+            reading = false;
+        }, 200);  //200 works fine for me but you can adjust it
+    }
+});
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          code is: <input defaultValue={UPC}/>
-        </p>
+          </p>
+          <br/>
+          <h2>UPC is: </h2>
+          <input defaultValue={UPC}/>
+          <p>upc: {UPC}</p>
+
+          <h2>code: </h2>>
+          <p>code: {code}</p>
+          <input defaultValue={code}/>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
