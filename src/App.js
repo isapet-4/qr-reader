756b7ff30d90bb22b5c2dbfc1c3cc0b3723c5f65
Isak Pettersson <isak.pettersson@apoteket.se>
2024-10-18 10:31:59 +0200

    add input field

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          code is: <input>{UPC}</input>
+          code is: <input defaultValue={UPC}/>
         </p>
         <a
           className="App-link"
