81ae3745e0c0d5209c9b6ddef5f477ac44a25142
Isak Pettersson <isak.pettersson@apoteket.se>
2024-10-18 14:11:36 +0200

    remove more

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import './App.css';
 import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  document.addEventListener('textInput', function (e){
-    if(e.data.length >= 6){
-        console.log('IR scan textInput', e.data);
-        document.getElementById("second").innerHTML = e.data
-        document.getElementById("scanner").value = e.data
-        e.preventDefault();
-    }
-});
+
 
 let code = "";
 let reading = false;
@@ -18,30 +11,40 @@ document.addEventListener('keypress', e => {
   //usually scanners throw an 'Enter' key at the end of read
    if (e.key === "ENTER") {
     document.getElementById("third").innerHTML = code
-   
+        //   if(code.length > 25) {
+        //     console.log(code);
+        //     /// code ready to use          
+        //     document.getElementById("second").innerHTML = code
+      
+        //     code = "";
+        //  }
     } else {
         code += e.key; //while this is not an 'enter' it stores the every key            
         document.getElementById("first").innerHTML = code
     }
 
-    if(!reading) {
-        reading = true;
-        setTimeout(() => {
-            code = "";
-            reading = false;
-        }, 200);  //200 works fine for me but you can adjust it
-    }
+    //run a timeout of 200ms at the first read and clear everything
+    // if(!reading) {
+    //     reading = true;
+    //     setTimeout(() => {
+    //         code = "";
+    //         reading = false;
+    //     }, 200);  //200 works fine for me but you can adjust it
+    // }
 });
 
 
   return (
     <div className="App">
       <header className="App-header">
-       
-      <img src={logo} className="App-logo" alt="logo" />
-        <div id="first" title='first'></div>
-        <div id="second" title='second'></div>
-        <div id="third" title='third'></div>
+        <img src={logo} className="App-logo" alt="logo" />
+      
+          <br/>
+         
+        first: <div id="first" ></div>
+        second: <div id="second"></div>
+        third: <div id="third"></div>
+        <div>{code}</div>
 
         <a
           className="App-link"
