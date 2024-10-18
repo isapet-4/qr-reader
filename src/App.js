e01107632fc87799f159ffbbd6552e01f8ab24a0
Isak Pettersson <isak.pettersson@apoteket.se>
2024-10-18 11:24:19 +0200

    t

--- src/App.js
+++ src/App.js
@@ -11,6 +11,33 @@ function App() {
     }
 });
 
+let code = "";
+let reading = false;
+
+document.addEventListener('keypress', e => {
+  //usually scanners throw an 'Enter' key at the end of read
+   if (e.key === 13) {
+          if(code.length > 5) {
+            console.log(code);
+            /// code ready to use                
+            code = "";
+         }
+    } else {
+        code += e.key; //while this is not an 'enter' it stores the every key            
+        document.getElementById("first").innerHTML = e.key
+
+    }
+
+    //run a timeout of 200ms at the first read and clear everything
+    if(!reading) {
+        reading = true;
+        setTimeout(() => {
+            code = "";
+            reading = false;
+        }, 200);  //200 works fine for me but you can adjust it
+    }
+});
+
 
   return (
     <div className="App">
@@ -29,6 +56,7 @@ function App() {
         <input type="text" id="scanner" placeholder="scanner"></input>
         <div id="first"></div>
         <div id="second"></div>
+        <div>{code}</div>
 
         <a
           className="App-link"
